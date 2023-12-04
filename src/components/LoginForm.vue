17b3afa170d1aa9fd576eaa860c121c43d8b925d
Pranav Chaudhari <pranav7.jobs@gmail.com>
2023-12-04 16:18:07 +0530

    login email regex

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -27,8 +27,9 @@ export default {
     const { handleSubmit } = useForm({
       validationSchema: {
         email(value) {
-          if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true;
-          return "Must be a valid e-mail.";
+          if (!value) return "This field is required.";
+          else if (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value)) return true;
+          else return "Must be a valid e-mail.";
         },
       },
     });
