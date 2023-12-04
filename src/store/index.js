0ea08437783f92eb7996751f2073a759179f2735
Pranav Chaudhari <pranav7.jobs@gmail.com>
2023-12-04 16:16:23 +0530

    implemented pinia store

--- src/store/index.js
+++ src/store/index.js
@@ -1,45 +1,81 @@
 import { defineStore } from "pinia";
-import { useStorage } from '@vueuse/core';
+import { useStorage } from "@vueuse/core";
 
 export const useUserStore = defineStore({
-    id: "user",
-    state: () => ({
-        isLoggedIn: useStorage('isLoggedIn', false),
-        email: useStorage('email', null),
-        chats: useStorage('chats',[
-            {
-              name: "Coding Pals",
-              created_at: "4Feb2022",
-              messages: [],
-            },
-            {
-              name: "Scouting",
-              created_at: "4Feb2022",
-              messages: [],
-            },
-            {
-              name: "Sports",
-              created_at: "4Feb2022",
-              messages: [],
-            },
-            {
-              name: "Music",
-              created_at: "4Feb2022",
-              messages: [],
-            },
-          ])
-    }),
-    actions: {
-        login(email) {
-            this.email = email;
-            this.isLoggedIn = true;
-        },
-        logout() {
-            this.email = null;
-            this.isLoggedIn = false;
-        },
-        addChat(userObj) {
-            this.chats.push(userObj);
-        }
-    }
-})
+  id: "user",
+  state: () => ({
+    isLoggedIn: useStorage("isLoggedIn", false),
+    email: useStorage("email", null),
+    chats: useStorage("chats", [
+      {
+        name: "Coding Pals",
+        created_at: "4Feb2022",
+        messages: [
+          {
+            created_at: "12:34AM",
+            msg: "Message display",
+          },
+        ],
+      },
+      {
+        name: "Scouting",
+        created_at: "4Feb2022",
+        messages: [
+          {
+            created_at: "12:35AM",
+            msg: "Message display",
+          },
+          {
+            created_at: "12:35AM",
+            msg: "Neque sagittis at ut consequat dolor odio penatibus lacus vel.",
+          },
+          {
+            created_at: "12:35AM",
+            msg: "Viverra arcu fusce suspendisse tortor. Euismod lorem sed libero gravida.",
+          },
+          {
+            created_at: "12:35AM",
+            msg: "Amet, in gravida aliquam duis maecenas",
+          },
+        ],
+      },
+      {
+        name: "Sports",
+        created_at: "4Feb2022",
+        messages: [
+          {
+            created_at: "12:36AM",
+            msg: "Message display",
+          },
+          {
+            created_at: "12:36AM",
+            msg: "Viverra arcu fusce suspendisse tortor. Euismod lorem sed libero gravida."
+          }
+        ],
+      },
+      {
+        name: "Music",
+        created_at: "4Feb2022",
+        messages: [
+          {
+            created_at: "12:37AM",
+            msg: "Message display",
+          }
+        ],
+      },
+    ]),
+  }),
+  actions: {
+    login(email) {
+      this.email = email;
+      this.isLoggedIn = true;
+    },
+    logout() {
+      this.email = null;
+      this.isLoggedIn = false;
+    },
+    addChat(userObj) {
+      this.chats.push(userObj);
+    },
+  },
+});
